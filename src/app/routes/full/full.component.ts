24522d1061a46c5d2d27d7fdbe9785e7a53900fb
Mark Thompson <mthompson@bitovi.com>
2022-02-16 20:14:02 -0800

    test

--- src/app/routes/full/full.component.ts
+++ src/app/routes/full/full.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./full.component.scss'],
 })
 export class FullComponent {
-    constructor() {}
+    constructor() {} // asdfasdfasdfasdfasdfasdfasfasfsfasdfasdfasdfasdfasdfasdfasfasfsfasdfasdfasdfasdfasdfasdfasfasfsfasdfasdfasdfasdfasdfasdfasfasfsf
 }
